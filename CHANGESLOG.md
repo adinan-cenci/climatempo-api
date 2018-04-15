c35ff2397147976331b49fc2952cab5619d59a29
AdinanCenci <adinancenci@gmail.com>
2018-04-15 10:28:41 -0300

    3.1.0
    Added support to request weather history

--- CHANGESLOG.md
+++ CHANGESLOG.md
@@ -1,6 +1,12 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## 3.2.0 - 2018-04-15
+
+### Added
+Now it is possible to request the weather history of a city.
+See examples.
+
 ## 3.0.1 - 2018-01-06
 
 ### Changed
---
+++ examples/4-history.php
@@ -0,0 +1,69 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+/*-----------------------------*/
+
+require '../src/Climatempo.php';
+require '../src/Wrapper.php';
+require '../src/Forecast.php';
+require '../src/History.php';
+
+use AdinanCenci\Climatempo\Climatempo;
+
+/*-----------------------------*/
+
+$token      = 'insert-your-token-here';
+$climatempo = new Climatempo($token);
+
+$id         = 3477; // São Paulo - SP
+$from       = date('Y-m-d', time() - 60 * 60 * 24 * 5); // 5 days ago
+
+
+try {
+    $history = $climatempo->history($id, $from);
+} catch (Exception $e) {
+    echo '<b>Error: </b>'.$e->getMessage();
+    die();
+}
+
+/*-----------------------------*/
+
+require 'resources/header.html';
+
+echo 
+"<h2>$history->name / $history->state - $history->country</h2>";
+
+
+foreach ($history->data as $log) {
+    echo 
+    "<table class=\"forecast\">
+        <caption>
+            $log->date &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; $log->dateBr &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; $log->timestamp
+        </caption>
+        <tbody>
+            <tr>
+                <th>Precipitation:</th>
+                <td>$log->precipitation mm</td>
+            </tr>
+            <tr>
+                <th>Source:</th>
+                <td>$log->rainInfoSource</td>
+            </tr>
+            <tr>
+                <th>Min Temperature:</th>
+                <td>$log->minTemp °C</td>
+            </tr>
+            <tr>
+                <th>Max Temperature:</th>
+                <td>$log->maxTemp °C</td>
+            </tr>
+            <tr>
+                <th>Source:</th>
+                <td>$log->tempInfoSource</td>
+            </tr>
+        </tbody>
+    </table>";
+}
+
+require 'resources/footer.html';
